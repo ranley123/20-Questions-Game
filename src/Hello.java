43315761d3dd3d6c504a13ad78f4f0f1b9b2bd67
ranley123 <yuhuan0109@gmail.com>
2020-02-21 15:53:14 +0000

    new first

--- src/Hello.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Hello {
-	public static void main(String[] args) throws IOException {
-		System.out.println("Hellow");
-		Game game = new Game("Question.txt");
-	}
-}
