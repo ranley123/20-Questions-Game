bec315169745d29801268ddadfcf5a99c7e848ce
ranley123 <yuhuan0109@gmail.com>
2020-02-25 04:17:16 +0000

    update output

--- src/BuildModel.java
+++ src/BuildModel.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.data.DataSet;
@@ -20,24 +21,22 @@ public class BuildModel {
 	int hidden_units_nums = 4;
 	NeuralNetwork<BackPropagation> nn;
 	BufferedReader reader;
-	DataSet training_set;
-	DataSet testing_set;
+	
 	ArrayList<String> labels;
-	
 	String model_filepath = "model.nnet";
-	
+	String animal_filepath = "animals.csv";
 	
 	private NeuralNetwork<BackPropagation> load_or_create_model(NeuralNetwork<BackPropagation> nn) throws IOException {
 		File model_file = new File(model_filepath);
 		if (model_file.exists()) {
 			nn = NeuralNetwork.load(model_filepath);
-			
 		}
 		else {
-			initialise_NN();
+			nn = initialise_NN();
 			
-			build_dataset_from_file("./src/data.csv");
+			DataSet training_set = build_dataset_from_file("./src/data.csv");
 			System.out.println("Training starts ...");
+			System.out.println(nn);
 			nn.learn(training_set);
 			System.out.println("Training completed.");
 			
@@ -46,20 +45,18 @@ public class BuildModel {
 		return nn;
 	}
 	
-	public void run(ArrayList<Double> answer_list) throws IOException {
+	public double[] run(ArrayList<Double> answer_list) throws IOException {
 		
 		nn = load_or_create_model(nn);
-		predict(answer_list);
-		
+		double[] outputs = predict(answer_list);
+		return outputs;
 	}
 	
-	private void build_dataset_from_file(String filepath) throws IOException {
+	private DataSet build_dataset_from_file(String filepath) throws IOException {
 		reader = new BufferedReader(new FileReader(filepath));
 		String line = "";
-		DataSet dataset = new DataSet(input_units_num, output_units_num);
-		training_set = new DataSet(input_units_num, output_units_num);
-		testing_set = new DataSet(input_units_num, output_units_num);
-		labels = new ArrayList<>();
+		DataSet training_set = new DataSet(input_units_num, output_units_num);
+		ArrayList<String> labels = new ArrayList<>();
 		
 		while ((line = reader.readLine()) != null){
 			String[] features = line.split(",");
@@ -69,13 +66,25 @@ public class BuildModel {
 			
 			ArrayList<Double> inputs = parse_in_outputs(input);
 			ArrayList<Double> outputs = parse_in_outputs(output);
-			dataset.addRow(new DataSetRow(inputs, outputs));
+			training_set.addRow(new DataSetRow(inputs, outputs));
 			
 			labels.add(label);
 		}
-
-		training_set = dataset;
-		
+//		System.out.println(training_set.size());
+		write_animals_to_csv(animal_filepath, labels);
+		return training_set;
+	}
+	
+	
+	
+	private void write_animals_to_csv(String animal_filepath, ArrayList<String> labels) throws IOException {
+		FileWriter writer = new FileWriter(animal_filepath);
+		for (String label: labels) {
+			writer.append(label);
+			writer.append("\n");
+		}
+		writer.flush();
+		writer.close();
 	}
 	
 	private ArrayList<Double> parse_in_outputs(String input){
@@ -88,25 +97,26 @@ public class BuildModel {
 	}
 
 	
-	private void initialise_NN() {
+	private NeuralNetwork<BackPropagation> initialise_NN() {
 		nn = new MultiLayerPerceptron(TransferFunctionType.SIGMOID, input_units_num, hidden_units_nums, output_units_num);
 		nn.setLearningRule(new MomentumBackpropagation());
 		MomentumBackpropagation learning_rule = (MomentumBackpropagation) nn.getLearningRule();
 		learning_rule.setLearningRate(0.1);
 		learning_rule.setMaxError(0.01);
+		return nn;
 	}
 	
-	private void predict(ArrayList<Double> answer) {
+	private double[] predict(ArrayList<Double> answer) {
 		DataSetRow row = new DataSetRow(answer);
+		labels = new ArrayList<>();
+		
 		nn.setInput(row.getInput());
 		nn.calculate();
+		
 		double[] output = nn.getOutput();
-        
-        String[] animal_list = new String[] {"lion", "cat", "dog", "bird", "fish", "snake", "sheep", "dolphin"};
-        int index = get_max_index(output);
-        System.out.println("You are thinking about: " + animal_list[index]);
-        
+        return output;
 	}
+
 	
 	private int get_max_index(double[] output) {
 		double max = output[0];
--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,14 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Game {
 	String filepath;
@@ -11,7 +16,11 @@ public class Game {
 	ArrayList<String> question_list;
 	ArrayList<Double> answer_list;
 	BuildModel model;
+	String animal_filepath = "animals.csv";
+	Scanner in;
 	
+	final int yes = 1;
+	final int no = 0;
 	/**
 	 * Read questions from file into a list
 	 * @param list 		- a list storing all questions
@@ -22,27 +31,46 @@ public class Game {
 		String question;
 		while ((question = reader.readLine()) != null) {
 			question_list.add(question);
-//			System.out.println("debug: " + question);
+			//			System.out.println("debug: " + question);
 		}
 	}
 	
-	private boolean is_valid_input(double input) {
+	private void read_animals_from_csv(String animal_filepath, ArrayList<String> animals) {
+		File labels = new File(animal_filepath);
+		try {
+			if (labels.exists()) {
+				String label = "";
+				reader = new BufferedReader(new FileReader(animal_filepath));
+				while((label = reader.readLine()) != null) {
+					if(label.length() > 0)
+						animals.add(label);
+				}
+				reader.close();
+			}
+		}
+		catch(FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private boolean is_valid_input(int input) {
 		return (input <= 1 && input >= 0);
 	}
-	
+
 	private void prompt_questions() {
 		answer_list = new ArrayList<Double>();
-		Scanner in = new Scanner(System.in);
 		System.out.println("Please enter 0 for no, 1 for yes !");
 		for (int i = 0; i < question_list.size(); i++) {
 			System.out.println("Q(" + (i + 1) + ")" + question_list.get(i));
-			double answer = in.nextDouble();
+			int answer = in.nextInt();
 			if (!is_valid_input(answer)) {
 				return;
 			}
-			answer_list.add(answer);
+			answer_list.add((double)answer);
 		}
-		in.close();
 	}
 
 	private void initialise() throws IOException {
@@ -51,6 +79,7 @@ public class Game {
 			reader  = new BufferedReader(new FileReader(filepath));
 			read_questions(question_list, reader);
 			model = new BuildModel();
+			in = new Scanner(System.in);
 		}
 		catch (FileNotFoundException e){
 			e.getMessage();
@@ -59,17 +88,63 @@ public class Game {
 			i.getMessage();
 		}
 		reader.close();
-
+		
 	}
-	
+
 	public void run() throws IOException {
 		prompt_questions();
-		model.run(answer_list);
+		double[] outputs = model.run(answer_list);
+		get_answer(outputs);
+		in.close();
 	}
 
+	private void get_answer(double[] outputs) {
+		ArrayList<String> labels = new ArrayList<String>();
+		Map<String, Double> map = new HashMap<>();
+		ValueComparator bvc = new ValueComparator(map);
+		Map<String, Double> sorted_map = new TreeMap<>(bvc);
+		
+		read_animals_from_csv(animal_filepath, labels);
+		for (int i = 0; i < outputs.length; i++) {
+			map.put(labels.get(i), outputs[i]);
+		}
+		sorted_map.putAll(map);
+		
+		for (Map.Entry<String, Double> entry: sorted_map.entrySet()) {
+			System.out.println("You are think of: " + entry.getKey());
+			System.out.println("Please enter 1 for yes, 0 for no");
+			int answer = in.nextInt();
+			if (is_valid_input(answer)) {
+				if (answer == yes)
+					break;
+				else
+					continue;
+			}
+		}
+//		System.out.println("You are thinking about: " + labels.get(index));
+	}
+	
 	public Game(String filepath) throws IOException {
 		this.filepath = filepath;
 		initialise();
 	}
 
+}
+
+class ValueComparator implements Comparator<String> {
+    Map<String, Double> base;
+
+    public ValueComparator(Map<String, Double> base) {
+        this.base = base;
+    }
+
+    // Note: this comparator imposes orderings that are inconsistent with
+    // equals.
+    public int compare(String a, String b) {
+        if (base.get(a) >= base.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys
+    }
 }
